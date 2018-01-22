56f693adab5d7b6297c2786d0982c58d720bdb11
C-E-P <c.elena.palmer@outlook.com>
2018-01-22 08:08:50 -0600

    Replies on Comments
    This commit adds rudimentary replies on comments, to be
    polished, made more DRY etc.

--- js/Comment.js
+++ js/Comment.js
@@ -8,7 +8,10 @@ export default class Comment extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {comment: props.comment};
+    this.state = {comment: props.comment, user: props.user};
+    this.showReply = this.showReply.bind(this);
+    this.addComment = this.addComment.bind(this);
+    this.updateNewCommentText = this.updateNewCommentText.bind(this);
   }
   componentWillMount() { 
     //do things
@@ -16,6 +19,35 @@ export default class Comment extends React.Component {
   componentWillUpdate(nextProps, nextState)
   {
   }
+  addComment(event)
+  {
+    if (this.state.newCommentText == null || this.state.newCommentText == "") return;
+    var commentUser = this.state.user != null && this.state.user != "" ? this.state.user : "Anonymous";
+    var newComment = {text: this.state.newCommentText, id: Math.random(), userName: commentUser, comments: [],
+      parentCommentId: this.state.comment.id};
+    var original = this.state.comment;
+    original.comments.push(newComment);
+    this.setState({
+      comment: original,
+      newCommentText: "",
+      showReply: false
+    });
+  }
+  updateNewCommentText(event)
+  {
+    this.setState(
+    {
+      newCommentText: event.target.value
+    })
+  }
+  showReply(event)
+  {
+    this.setState(
+    {
+      showReply: true
+    })
+  }
+
   render() {
     return (
       <div className="panel panel-default">
@@ -23,7 +55,22 @@ export default class Comment extends React.Component {
         <div className="panel-body">
             {this.state.comment.text}
           <div className="row">
-            <button className="btn btn-link">Reply</button>
+            <button className="btn btn-link" onClick={this.showReply}>Reply</button>
+          </div>
+          <div className={this.state.showReply ? "viewer-creator" : "viewer"}>
+            <div className="row">
+              <div className="col-md-12">
+                <textarea value={this.state.newCommentText} rows="3"
+                    onChange={this.updateNewCommentText} className="form-control"></textarea>
+                
+              </div>
+            </div>
+            <br/>
+            <div className="row">
+              <div className="col-md-12">
+                <button onClick={this.addComment}>Add Reply</button>
+              </div>
+            </div>  
           </div>
           <div className="row">
             {this.state.comment.comments.map(comment => 
