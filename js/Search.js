2fa9cc44a333ed2ccf9e759953c1f7164ba63d47
C-E-P <c.elena.palmer@outlook.com>
2018-02-18 18:18:10 -0600

    Advanced search controls + method stubs

--- js/Search.js
+++ js/Search.js
@@ -57,6 +57,31 @@ export default class Search extends React.Component {
     })
   }
 
+  updateSearchType(event, key)
+  {
+    console.log(key)
+  }
+
+  updateWorkFields(event, key)
+  {
+    console.log(key)
+  }
+
+  updateBookmarkFields(event, key)
+  {
+    console.log(key)
+  }
+
+  updateSearchWorks(event)
+  {
+    console.log(event.target.value)
+  }
+
+  updateSearchBookmarks(event)
+  {
+    console.log(event.target.value)
+  }
+
   render() {
     return (
       <div className="container">
@@ -76,22 +101,84 @@ export default class Search extends React.Component {
         <br/>
         {this.state.showAdvancedSearch ?
           <div>
-            <div className="row">
-              <div className="col-md-4">
-                work type toggles
+            <div className="panel panel-default">
+              <div className="panel-heading">General Search</div>
+                <div className="panel-body">
+                  <div className="row">
+                    <div className="col-sm-2">
+                      <input type="checkbox" id="searchTypeOne" onChange={evt => this.updateSearchType(evt, 1)}/>  Search work type 1?
+                    </div>
+                    <div className="col-sm-2">
+                      <input type="checkbox" id="searchTypeTwo" onChange={evt => this.updateSearchType(evt, 2)}/>  Search work type 2?
+                    </div>
+                    <div className="col-sm-2">
+                      <input type="checkbox" id="searchTypeThree" onChange={evt => this.updateSearchType(evt, 3)}/>  Search work type 3?
+                    </div>
+                    <div className="col-sm-2">
+                        <input type="checkbox" id="searchWorks" onChange={evt => this.updateSearchWorks(evt)}/>  Search works?
+                    </div>
+                    <div className="col-sm-4">
+                      <input type="checkbox" id="searchBookmarks" onChange={evt => this.updateSearchBookmarks(evt)}/>  Search bookmarks?
+                    </div>          
+                  </div>
               </div>
-              <div className="col-md-4">
-                work/bookmark toggles
-              </div>            
             </div>
-            <div className="row">
-              <div className="col-md-6">
-                work field to search toggles - title, creator, summary, tags, text
-              </div>
-              <div className="col-md-6">
-                bookmark field to search toggles - title, creator, curator, work summary, curator description, tags, curator rating
-              </div>
+            
+
+            <div className="panel panel-default">
+              <div className="panel-heading">Work Search</div>
+                <div className="panel-body">
+                  <div className="row">
+                    <div className="col-sm-2">   
+                      <input type="checkbox" id="searchTitle" onChange={evt => this.updateWorkFields(evt, 1)} checked={this.state.searchTitle}/>  Search title? 
+                    </div>
+                    <div className="col-sm-2">  
+                      <input type="checkbox" id="searchCreator" onChange={evt => this.updateWorkFields(evt, 2)} checked={this.state.searchCreator}/>  Search creator?
+                    </div>                    
+                    <div className="col-sm-2">  
+                      <input type="checkbox" id="searchTags" onChange={evt => this.updateWorkFields(evt, 4)} checked={this.state.searchTags}/>  Search tags?
+                    </div>
+                    <div className="col-sm-2"> 
+                      <input type="checkbox" id="searchText" onChange={evt => this.updateWorkFields(evt, 5)} checked={this.state.searchText}/>  Search text?
+                    </div>   
+                    <div className="col-sm-4">  
+                      <input type="checkbox" id="searchSummary" onChange={evt => this.updateWorkFields(evt, 3)} checked={this.state.searchSummary}/>  Search summary?
+                    </div>           
+                  </div>
+                </div>
             </div>
+            <div className="panel panel-default">
+              <div className="panel-heading">Bookmark Search</div>
+                <div className="panel-body">
+                  <div className="row">
+                    <div className="col-sm-2">
+                        <input type="checkbox" id="searchTitle" onChange={evt => this.updateBookmarkFields(evt, 1)} checked={this.state.searchTitle}/>  Search title?
+                    </div>
+                    <div className="col-sm-2">
+                        <input type="checkbox" id="searchCreator" onChange={evt => this.updateBookmarkFields(evt, 2)} checked={this.state.searchCreator}/>  Search creator?
+                    </div>
+                    <div className="col-sm-2">
+                        <input type="checkbox" id="searchCurator" onChange={evt => this.updateBookmarkFields(evt, 3)} checked={this.state.searchCurator}/>  Search curator?
+                    </div>
+                    <div className="col-sm-4">
+                        <input type="checkbox" id="searchSummary" onChange={evt => this.updateBookmarkFields(evt, 4)} checked={this.state.searchSummary}/>  Search summary?
+                    </div>                    
+                  </div>
+                  <div className="row">                      
+                    <div className="col-sm-2">
+                        <input type="checkbox" id="searchTags" onChange={evt => this.updateBookmarkFields(evt, 6)} checked={this.state.searchTags}/>  Search tags?
+                    </div>
+                    <div className="col-sm-2">
+                      <input type="checkbox" id="searchRating" onChange={evt => this.updateBookmarkFields(evt, 7)} checked={this.state.searchRating}/>  Search rating?
+                    </div>
+                    <div className="col-sm-4">
+                      <input type="checkbox" id="searchDescription" onChange={evt => this.updateBookmarkFields(evt, 5)} checked={this.state.searchDescription}/>  Search description?
+                    </div>
+                  </div>
+                </div>
+            </div>
+            
+            
           </div>
 
           : <div/>}
