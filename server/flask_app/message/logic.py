56bd1479b1fcf9e6a6674aea1bb1258e0d1c80c7
C-E-P <c.elena.palmer@outlook.com>
2018-03-11 16:54:20 -0500

    message unit tests, functions for crud

--- server/flask_app/message/logic.py
+++ server/flask_app/message/logic.py
@@ -4,3 +4,82 @@ import json
 from .. import db
 from ..work import views
 from ..models import Work, Chapter, Tag, User, TagType, Bookmark, BookmarkLink, Message
+
+def add_message(json):
+	message = Message(to_user_id = json["to_user"], from_user_id = json["from_user"]["user_id"],
+		message_subject=json["message_subject"],message_content=json["message_content"])
+	if "parent_id" in json:
+		parent_message = Message.query.filter_by(id = json["parent_id"]).first()
+		parent_message.replies.append(message)
+		db.session.add(parent_message)
+	db.session.add(message)
+	db.session.commit()
+	return message.id
+
+def delete_message(message_id):
+	try:
+		Message.query.filter_by(id=message_id).delete()
+		db.session.commit()
+	except:
+		#todo log
+		return
+
+def update_read_status(message_id, status):
+	message = Message.query.filter_by(id=message_id).first()
+	if message is not None:
+		message.message_read = status
+		db.session.add(message)
+		db.session.commit()
+		return "Success"
+	else:
+		return None
+
+def mark_all_read(user_id):
+	user = User.query.filter_by(id=user_id).first()
+	for message in user.received_messages:
+		update_read_status(message.id, True)
+
+def get_message(message_id):
+	message = Message.query.filter_by(id=message_id).first()
+	if message is not None:
+		return build_message(json)
+	else:
+		return None
+
+def get_messages_by_sender(sender_id):
+	messages = Message.query.filter_by(from_user_id=sender_id)
+	if messages is not None:
+		messages_json = []
+		for message in messages:
+			messages_json.append(build_message(message))
+		return messages_json
+	else:
+		return None
+
+def get_messages_by_sendee(sendee_id):
+	messages = Message.query.filter_by(to_user_id=sendee_id)
+	if messages is not None:
+		messages_json = []
+		for message in messages:
+			messages_json.append(build_message(message))
+		return messages_json
+	else:
+		return None
+
+def build_message(message):
+	from_user = User.query.filter_by(id=message.from_user_id).first()
+	built = {}
+	built["to_user"] = message.to_user_id
+	built["from_user"] = {"user_id": message.from_user_id, "name": from_user.username}
+	built["message_subject"] = message.message_subject
+	built["message_content"] = message.message_content
+	built["replies"] = list(build_replies(message.replies))
+	built["parent_id"] = message.parent_message
+	return built
+
+def build_replies(replies):
+	json = []
+	for reply in replies:
+		built = build_message(reply)
+		json.append(built)
+	return json
--- server/tests/test_message.py
+++ server/tests/test_message.py
@@ -1,70 +1,78 @@
 import unittest
 
 from server.flask_app import db
-from server.flask_app.models import Work, User, TagType, Chapter, Tag, Comment, Bookmark, Message
-from server.flask_app.bookmark import logic as bookmark
-from server.flask_app.work import views as work
+from server.flask_app.models import User, Message
+from server.flask_app.message import logic as message
 from server.tests.base import BaseTestCase
 import json
 
 
 class TestMessage(BaseTestCase):
     def test_add_message(self):
-        data = self.build_data(True, False, True)
-        
-        self.assertTrue(True)
+        data = self.build_data()
+        new_id = message.add_message(data)
+        new_message = Message.query.filter_by(id=new_id).first()
+        self.assertTrue(new_message.message_subject == "test")
+
+    def test_delete_message(self):
+        data = self.build_data()
+        new_id = message.add_message(data)
+        message.delete_message(new_id)
+        self.assertTrue(len( Message.query.all()) == 0)
+
+    def test_mark_read_message(self):
+        data = self.build_data()
+        new_id = message.add_message(data)    
+        second_id = message.add_message(data)    
+        message.update_read_status(new_id, True)
+        new_message = Message.query.filter_by(id=new_id).first()
+        second_message = Message.query.filter_by(id=second_id).first()
+        self.assertTrue(new_message.message_read == True)
+        self.assertTrue(second_message.message_read == False)
+
+    def test_mark_all_read_message(self):
+        data = self.build_data()
+        new_id = message.add_message(data)    
+        second_id = message.add_message(data)    
+        message.mark_all_read(1)
+        new_message = Message.query.filter_by(id=new_id).first()
+        second_message = Message.query.filter_by(id=second_id).first()
+        self.assertTrue(new_message.message_read == True)
+        self.assertTrue(second_message.message_read == True)
+
+    def test_add_message_reply(self):
+        data = self.build_data()
+        new_id = message.add_message(data)   
+        data["parent_id"] = new_id 
+        second_id = message.add_message(data)    
+        new_message = Message.query.filter_by(id=new_id).first()
+        self.assertTrue(new_message.replies[0].id == second_id)
 
 
-    def build_data(self, build_tags, build_comments, build_links):
-        data = {}
-
-        data["work_id"] = 1
-        
-        if build_tags == False:
-            data["tags"] = []
-        else:
-
-            tagType = TagType(label='one')
-            db.session.add(tagType)
-
-            tagType = TagType(label='two')
-            db.session.add(tagType)
-            tag_one = {}
-            tag_one['label'] = "blah"
-            tag_one['id'] = 1
-            tag_one['tags'] = ['one', 'two', 'three']
-
-
-            tag_two = {}
-            tag_two['label'] = "bleh"
-            tag_two['id'] = 2
-            tag_two['tags'] = ['one', 'two', 'four']
-            data["tags"] = [tag_one, tag_two]
-        data["curator_title"] = "A Fic I Read"
-        data["description"] = "This was fine I guess..."
-        data["rating"] = 3
-        if build_links == False:
-            data["links"] = []
-        else:
-            link_one = {}
-            link_one["link"] = "google.com",
-            link_one["text"] = "Goog"
-
-            link_two = {}
-            link_two["link"] = "uber.com",
-            link_two["text"] = "Eat The Rich"
-
-            data["links"] = [link_one, link_two]
+    def build_data(self):
+        built = {}
+        built["to_user"] = 1
+        built["from_user"] = {"user_id": 1, "name": "elena"}
+        built["message_subject"] = "test"
+        built["message_content"] = "this is just a test message, calm down"
+        built["replies"] = []
 
         self.add_user()
 
-        return data
+        return built
+        
     def add_user(self):
         user = User(
             email='test@test.com',
             password='test'
         )
         db.session.add(user)
+        userTwo = User(
+            email='test2@test.com',
+            password='test'
+        )
+        db.session.add(user)
+        db.session.add(userTwo)
         db.session.commit()
 
 if __name__ == '__main__':
